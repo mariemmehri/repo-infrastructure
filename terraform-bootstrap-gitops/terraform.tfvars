2b7fe574fe502e74a8d646aec8622e1616d0c6c0
maryem-mehri <mehri.mariem@esprit.tn>
2026-04-12 02:00:57 +0100

    Add infra terraform stacks

---
+++ terraform-bootstrap-gitops/terraform.tfvars
@@ -0,0 +1,7 @@
+resource_group_name = "rg-todo-app"
+cluster_name        = "myclusterAKS"
+
+# Optional overrides for GitOps source
+config_repo_url      = "https://github.com/mariemmehri/repo-config"
+config_repo_revision = "main"
+child_apps_path      = "apps/children"
--- terraform-todo/outputs.tf
+++ terraform-todo/outputs.tf
@@ -27,5 +27,6 @@ output "argocd_portforward_command" {
 # Commande pour récupérer le mot de passe admin ArgoCD
 output "argocd_password_command" {
   description = "Commande pour récupérer le mot de passe admin ArgoCD"
-  value       = "kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}""
+  value       = "kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath=\"{.data.password}\""
+  #kubectl -n argocd exec -it deployment/argocd-server -- argocd admin initial-password
 }
